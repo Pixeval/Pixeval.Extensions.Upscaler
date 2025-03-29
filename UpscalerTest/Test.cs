64c6ff8357e51afdf8d55990d30e61e748f47f23
Poker <poker_sang@outlook.com>
2025-03-29 17:46:40 +0800

    update

--- UpscalerTest/Test.cs
+++ UpscalerTest/Test.cs
@@ -9,9 +9,9 @@ public sealed class Test
     [TestMethod]
     public async Task TestMethod()
     {
-        var host = new ExtensionsHost();
+        IExtensionsHost host = new ExtensionsHost();
         const string prefix = @"C:\Users\poker\AppData\Local\Packages\PokerKo.4454907E5DDB5_0wpjzgvbyjvyr\";
-        host.Initialize("zh-hans", prefix + "TempState", prefix + @"LocalState\Extensions");
+        host.Initialize("zh-Hans", prefix + "TempState", prefix + @"LocalState\Extensions");
         await using var stream = Helper.OpenAsyncRead(@"..\..\..\..\Pixeval.Extensions.Upscaler\Resources\logo.png");
         await using var upscaler = new Upscaler();
         var result = await upscaler.UpscaleAsync(stream.ToIStream());
